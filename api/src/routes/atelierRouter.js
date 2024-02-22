d0c0fe8ebb63ad2125d2103eeed20a344c853d1c
NicoRiri <nicolate.bernardet@gmail.com>
2024-02-22 14:00:15 +0100

    route

--- api/src/routes/atelierRouter.js
+++ api/src/routes/atelierRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {getAtelierAction} from "../actions/GetAtelierAction.js";
+import {getAtelierByIdAction} from "../actions/GetAtelierByIdAction.js";
 
 const router = express.Router();
 
@@ -9,4 +10,12 @@ router
         getAtelierAction(req, res, next)
     })
     .all((req, res, next) => next(405)); //method not allowed
+
+router
+    .route("/:id")
+    .get((req, res, next) => {
+        getAtelierByIdAction(req, res, next)
+    })
+    .all((req, res, next) => next(405)); //method not allowed
+
 export default router;
