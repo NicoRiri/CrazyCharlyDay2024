89a3b59f5942e8896b1445e5c0e783a379e89e20
NicoRiri <nicolate.bernardet@gmail.com>
2024-02-22 16:09:53 +0100

    feat: GET voeux

--- api/src/routes/voeuxRouter.js
+++ api/src/routes/voeuxRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
 import {PostVoeuxAction} from "../actions/PostVoeuxAction.js";
+import {GetVoeuxAction} from "../actions/GetVoeuxAction.js";
 
 const router = express.Router();
 
 router
     .route("/")
+    .get((req, res, next) => {
+        GetVoeuxAction(req, res, next)
+    })
     .post((req, res, next) => {
         PostVoeuxAction(req, res, next)
     })
