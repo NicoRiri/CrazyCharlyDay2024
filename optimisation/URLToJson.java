97a5d864f0083b92b52567061121e59dabdcc996
Egario-akier <thierry.dissidia@gmail.com>
2024-02-22 16:02:23 +0100

    URL vers JSON en java

---
+++ optimisation/URLToJson.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class URLToJson {
+    private static String lienDuSite = "http://docketu.iutnc.univ-lorraine.fr:35652";
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, JSONException {
+        JSONObject json = readJsonFromUrl(lienDuSite + "/atelier/1");
+        System.out.println(json.toString());
+        System.out.println(json.get("id"));
+    }
+}
