c3655a44b47b2a5f0b81f0c990e02115ff056361
NicoRiri <nicolate.bernardet@gmail.com>
2024-02-22 15:24:14 +0100

    refactor : retour

--- api/src/actions/PostAtelierAction.js
+++ api/src/actions/PostAtelierAction.js
@@ -3,8 +3,7 @@ import SsAtelier from "../service/SsAtelier.js";
 export async function PostAtelierAction(req,res,next){
     try {
         const atelier = await SsAtelier.postAtelier(req, res, next)
-        console.log(atelier)
-        res.json({data:atelier})
+        res.json({message: "L'atelier a bien été créé"})
     }catch(e){
         console.error(e);
         next(500);
