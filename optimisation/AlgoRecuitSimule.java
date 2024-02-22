a879c90562c3a52ab73412d093f96f093ed0865f
miesch3u <nathanaelmiesch@gmail.com>
2024-02-22 16:43:18 +0100

    bidouille dans l'algo

--- optimisation/AlgoRecuitSimule.java
+++ optimisation/AlgoRecuitSimule.java
@@ -4,32 +4,44 @@ public class AlgoRecuitSimule {
   private double energie;
   private int nbEtapes;
   private final int nbMaxEtapes = 100_000;
-  private final double energieMin = 100_000;
+  private int eMax;
 
   public AlgoRecuitSimule(){
     //TODO
   }
 
+  private double getTemp(int nbEtapes, int MaxEtapes){
+    return 1 - (double)nbEtapes/MaxEtapes;
+  }
+
+  private double getProba(double diffEn, double temp){
+    return Math.exp(-diffEn/temp);
+  }
+
   public State algo(List<Candidat> candidats, List<Atelier> ateliers){
+    eMax = 20*candidats.size();
     State etat = State.getRandomState(candidats, ateliers);
     State etatOptimal = etat;
-    int meilleureEnergie = etatOptimal.getScore();
+    int meilleureEnergie = eMax/etatOptimal.getScore();
 
     State tempEtat;
-    int tempEnergie;
-    while (nbEtapes < nbMaxEtapes && energie < energieMin){
+    int tempEnergie = eMax/etat.getScore();
+    int energieDAvant;
+    do {
+      energieDAvant = tempEnergie;
       tempEtat = State.getNeighbourState(etatOptimal);
-      tempEnergie = tempEtat.getScore();
-      if(tempEnergie < meilleureEnergie || Math.random() < Math.exp((meilleureEnergie - tempEnergie) / energie)){
+      tempEnergie = eMax/tempEtat.getScore();
+      if(tempEnergie < meilleureEnergie || Math.random() <getProba(energieDAvant - tempEnergie, getTemp(nbEtapes, nbMaxEtapes))){
         etatOptimal = tempEtat;
         meilleureEnergie = tempEnergie;
       }
       if(tempEnergie < meilleureEnergie){
+        System.out.println("Nouvelle énergie : " + tempEnergie);
         etatOptimal = tempEtat;
         meilleureEnergie = tempEnergie;
       }
       nbEtapes++;
-    }
+    } while (nbEtapes < nbMaxEtapes && energie < eMax);
     return etatOptimal;
   }
 }
